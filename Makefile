1cd74b4dd8afb01179364874a14359373b8b6e7b
nakario <nakario.jp@gmail.com>
2019-09-23 19:21:26 +0900

    Avoid an error when the dir exists

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
+.PHONY: all
+all: update test
+
+.PHONY: update
+update:
+	@git pull origin master
+
 .PHONY: test
 test: .test
 	@ln -snfv $(PWD)/$^ $(HOME)/$^
--- init.sh
+++ init.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+DOTFILES_TEST_URL="https://github.com/nakario/dotfiles-test.git"
+DOTFILES_TEST_DIR=$HOME/dotfiles-test
+
 if [[ ! -x $(command -v git) ]]; then
   echo "git not found"
   exit
 fi
 
-git clone https://github.com/nakario/dotfiles-test.git $HOME/dotfiles-test
-cd $HOME/dotfiles-test
-make test
+if [[ ! -d "${DOTFILES_TEST_DIR}" ]]; then
+  git clone "${DOTFILES_TEST_URL}" "${DOTFILES_TEST_DIR}"
+fi
+cd "${DOTFILES_TEST_DIR}"
+make all
